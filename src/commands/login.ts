d2ad842ffdeb709f9531fefb4dada9dcb9a33ae0
Sherlock <135939370+sherlock-redhorse@users.noreply.github.com>
2024-11-03 23:35:37 -0600

    Revert "feat!: stallion 2.0 cli update (#15)"
    This reverts commit f27b4ab9875d2bdc4b34cee870b569d1c64408b2.

--- src/commands/login.ts
+++ src/commands/login.ts
@@ -1,7 +1,13 @@
 import Command, { CommandArgs } from '../command-line/command';
 import { help } from '../command-line/option-decorators';
-import { CommandResult } from '../command-line/command-result';
+import { CommandResult, ErrorCodes, failure, success } from '../command-line/command-result';
+import { out, prompt } from '../interaction-output';
 
+const opener = require('opener');
+import * as os from 'os';
+import { Endpoints } from '../apis/endpoints';
+import { tokenStore, TokenValueType } from '../token-store';
+import * as chalk from 'chalk';
 
 @help('Log in and save token')
 export default class Login extends Command {
@@ -10,6 +16,22 @@ export default class Login extends Command {
     }
 
     async runCheckIfLoggedIn(): Promise<CommandResult> {
-       return this.openLoginFlow()
+        out.text(`Opening your browser... ${os.EOL}• Visit ${Endpoints.CLI_LOGIN} and enter the code:`);
+        opener(Endpoints.CLI_LOGIN);
+        const token = await prompt('Access code from browser: ');
+        const data: TokenValueType = { id: null, token: token };
+        const status = await this.login(data);
+        if (!status) return failure(ErrorCodes.Exception, 'something went wrong');
+        out.text(chalk.green('\nToken saved, Login success!!'));
+        return success();
+    }
+
+    private async login(token: TokenValueType) {
+        try {
+            await tokenStore.set('ACCESS_TOKEN', token);
+            return true;
+        } catch (e) {
+            return false;
+        }
     }
 }
