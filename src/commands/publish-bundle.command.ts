d727151de20897a9cfd6f3fe743ebe0c7e24f2aa
Maximus Decimus Meridius <captainmaximusdecimus@gmail.com>
2025-12-29 11:31:12 +0530

    Chore: Added SourceMap Flag for keeping artifacts

--- src/commands/publish-bundle.command.ts
+++ src/commands/publish-bundle.command.ts
@@ -161,7 +161,7 @@ export class PublishBundleCommand extends BaseCommand {
       );
     }
 
-    if (keepArtifacts) {
+    if (keepArtifacts || sourcemap) {
       await runComposeSourcemapCommand(this.contentRootPath, bundleName);
       copyDebugId(path.join(this.contentRootPath, "sourcemaps", bundleName + ".packager.map"), path.join(this.contentRootPath, "sourcemaps", bundleName + ".map"));
       await this.keepArtifacts(this.contentRootPath, platform);
